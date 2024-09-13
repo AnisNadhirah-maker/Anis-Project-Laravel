ecd1e268eef99433996d2a028fcd973695dc235c
AnisNadhirah-maker <anisnadhirah481@gmail.com>
2024-09-13 21:54:45 +0700

    First Assignment
    First Assignment

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:title>{{$title}}</x-slot>
+    <h3 class="text-xl">You can contact me through: </h3>
+    <a href="https://instagram.com/anisnadh">Instagram</a>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:title>{{$title}}</x-slot>
+    <h3 class="text-xl">This is home page</h3>
+</x-layout>
